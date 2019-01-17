f6e137dd076a1a5eef3f5079342857b833842ab0
oberdev <qovalski@gmail.com>
2019-01-18 01:25:29 +0300

    #4 create base models

--- aircarts/admin.py
+++ aircarts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Aircart
+from .models import Plane, PlaneCompany
 # Register your models here.
-admin.site.register(Aircart)
+admin.site.register(Plane)
+admin.site.register(PlaneCompany)
---
+++ blocks/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Unit, UnitAction, UnitCreator
+
+admin.site.register(Unit)
+admin.site.register(UnitAction)
+admin.site.register(UnitCreator)
+
+# Register your models here.
