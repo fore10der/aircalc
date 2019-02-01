1ed282c2b7cbedab53ae206819ceb1e747a54cbd
oberdev <qovalski@gmail.com>
2019-02-02 01:32:33 +0300

    delete tests for useless functions

--- units/tests.py
+++ units/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from .models import Unit, UnitCreator
-from loader.utils import is_name_exist, is_source_exist
 
 class ModelExistTest(TestCase):
     def setUp(self):
@@ -8,7 +7,3 @@ class ModelExistTest(TestCase):
     def test_get_from_db(self):
         unit_creator = UnitCreator.objects.get(id=1)
         self.assertEqual(unit_creator.name,'ass')
-    def test_check_existed_from_db(self):
-        self.assertEqual(is_name_exist("hardbass",UnitCreator),False)
-    def test_check_not_existed_from_db(self):
-        self.assertEqual(is_name_exist("ass",UnitCreator),True)
