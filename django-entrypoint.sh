59d5847bec49f9d5e2d906576ea0b11d56d276e5
oberdev <qovalski@gmail.com>
2019-02-05 18:18:50 +0300

    impove docker script

--- django-entrypoint.sh
+++ django-entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 rm -rf $(find . -name migrations)
-python /code/manage.py makemigrations
+python manage.py makemigrations
 
 echo "MIGRATING DATABASE"
-python /code/manage.py migrate
+python manage.py migrate
 
 echo "RUN"
-python /code/manage.py runserver 0.0.0.0:8000
-
-
-# echo "CREATE ADMIN"
-# python /code/manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+python manage.py runserver 0.0.0.0:8000
