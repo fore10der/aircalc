01fb70e860a8fdef1bf8ff98635915e23902bf87
oberdev <qovalski@gmail.com>
2019-02-07 14:25:24 +0300

    refactror docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 rm -rf $(find . -name migrations)
-python manage.py makemigrations --settings=gss.settings.ci
+python manage.py makemigrations --settings=gss.settings.docker
 
 echo "MIGRATING DATABASE"
-python manage.py migrate --settings=gss.settings.ci
+python manage.py migrate --settings=gss.settings.docker
 
 echo "COLLECT STATIC"
-python manage.py collectstatic --no-input --settings=gss.settings.ci
+python manage.py collectstatic --no-input --settings=gss.settings.docker
 
 
 
