b2324b89b792b326e0215b5dd6fda786456176ed
oberdev <qovalski@gmail.com>
2019-02-07 14:25:49 +0300

    refactror docker

---
+++ gss/settings/docker.py
@@ -0,0 +1,15 @@
+from .base import *
+
+
+PAGE_CACHE_SECONDS = 1
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.getenv('SQL_USER', 'user'),
+        'PASSWORD': os.getenv('SQL_PASSWORD', 'password'),
+        'HOST': os.getenv('SQL_HOST', 'localhost'),
+        'PORT': os.getenv('SQL_PORT', '5432'),
+    }
+}
