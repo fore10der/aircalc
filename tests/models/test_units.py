17f14f3995c26e45bbc69312dfdac64088f85c83
oberdev <qovalski@gmail.com>
2019-02-08 01:33:53 +0300

    debug

--- tests/models/test_units.py
+++ tests/models/test_units.py
@@ -5,11 +5,11 @@ from units.models import Unit, UnitCreator, UnitAction
 
 class UnitModelsTests(TestCase):
     def setUp(self):
-        self.creator = UnitCreator.create(name="cool creator")
-        self.unit = Unit.create(number="cool unit", manufacturer=self.creator)
+        self.creator = UnitCreator.objects.create(name="cool creator")
+        self.unit = Unit.objects.create(number="cool unit", manufacturer=self.creator)
         self.actions = []
-        self.actions.append(UnitAction.create(date=date(2018,2,15), unit=self.unit, action_type=0))
-        self.actions.append(UnitAction.create(date=date(2020,1,23), unit=self.unit, action_type=1))
-        self.actions.append(UnitAction.create(date=date(2016,2,6), unit=self.unit, action_type=2))
+        self.actions.append(UnitAction.objects.create(date=date(2018,2,15), unit=self.unit, action_type=0))
+        self.actions.append(UnitAction.objects.create(date=date(2020,1,23), unit=self.unit, action_type=1))
+        self.actions.append(UnitAction.objects.create(date=date(2016,2,6), unit=self.unit, action_type=2))
     def test_unicode(self):
         self.assertEqual(True, True)
