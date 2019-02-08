dea1559353777ce24e7dac49e33afdfbac365313
oberdev <qovalski@gmail.com>
2019-02-08 13:04:38 +0300

    add special errors code

--- app/gss/views.py
+++ app/gss/views.py
@@ -2,3 +2,7 @@ from django.views.generic import TemplateView
 
 class AccessDeniedTemplate(TemplateView):
     template_name = 'error.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context, status=403)
