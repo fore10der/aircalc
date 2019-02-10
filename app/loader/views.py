98a90123700b8b1706bf471f32b8fa97b488058c
oberdev <qovalski@gmail.com>
2019-02-10 07:00:07 +0300

    modified upload view

--- app/loader/views.py
+++ app/loader/views.py
@@ -4,6 +4,9 @@ from .forms import TestFileForm
 from .models import UploadedFile
 from .utils import preprocess_xlsx, store_to_db
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.urls import reverse
+
 
 class UploadFileView(FormMixin,ListView):
     queryset = UploadedFile.objects.order_by('-upload_date')
@@ -15,3 +18,5 @@ class UploadFileView(FormMixin,ListView):
     def post(self, form):
         data = preprocess_xlsx(self.request.FILES['file_input'])
         store_to_db(data)
+        UploadedFile.objects.create(file=self.request.FILES['file_input'],uploader=self.request.user.username)
+        return HttpRequest(reverse('loader'))
