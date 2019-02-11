579d4bc1920c6b97b23cbec11cefa6002449623d
oberdev <qovalski@gmail.com>
2019-02-11 23:20:36 +0300

    done

---
+++ app/assets/src/js/history.js
@@ -0,0 +1,7 @@
+$('.upload_slider').slick({
+    dots: true,
+    vertical: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    verticalSwiping: true,
+  });
--- app/assets/src/js/index.js
+++ app/assets/src/js/index.js
@@ -1,1 +1,25 @@
-import "./dropzoneloader.js";
+import $ from "jquery";
+window.$ = $;
+import 'slick-carousel';
+
+let $status = $('.pager .pagenum')
+let $uploadSlider = $('.upload_slider')
+let $prev = $('.pager .prev')
+let $next = $('.pager .next')
+let $emptymark = $('.is_empty')
+
+if($uploadSlider.length && !$emptymark.length){
+  $uploadSlider.on('afterChange',(event, slick, currentSlide, nextSlide)=>{
+    let i = Math.floor(currentSlide/3)+1;
+    $status.text(i);
+  });
+  $uploadSlider.slick({
+      dots: false,
+      vertical: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      verticalSwiping: true,
+      prevArrow: $prev,
+      nextArrow: $next,
+    })
+}
