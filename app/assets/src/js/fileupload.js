1ac09fcc20baf5749f2cd3b38f3a73ce8943be81
oberdev <qovalski@gmail.com>
2019-02-19 03:24:29 +0300

    drag and drop frontend

--- app/assets/src/js/fileupload.js
+++ app/assets/src/js/fileupload.js
@@ -7,11 +7,26 @@ $(document).ready(()=>{
     const myDropzone = new Dropzone(dropzoneId, {
         url: "/upload/",
         paramName: "file",
+        maxFiles: 1,
+        clickable: document.querySelector(`${dropzoneId} button`),
         init: function () {
             this.on("success", function (file, response) {
               console.log(response);
             });
+            this.on("maxfilesexceeded", function(file) {
+              this.removeAllFiles();
+              this.addFile(file);
+            });
+            this.on("dragenter",function() {
+              console.log("start");
+            })
+            this.on("dragleave",function() {
+              console.log("end");
+            })
           }
     });
+    document.querySelector(`${dropzoneId} button`).addEventListener('click',(e)=>{
+      e.preventDefault()
+    })
 }
 })
