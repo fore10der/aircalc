db2a220c2f606583bd9679c148ea9c4ec9ecd1b9
oberdev <qovalski@gmail.com>
2019-02-25 17:32:45 +0300

    revert

--- app/assets/webpack.config.js
+++ app/assets/webpack.config.js
@@ -126,7 +126,7 @@ module.exports = {
       },
     ]),
         new HtmlWebPackPlugin({
-            template: "./src/report.html",
+            template: "./src/upload.html",
         filename: "./index.html"
         })
     ]
